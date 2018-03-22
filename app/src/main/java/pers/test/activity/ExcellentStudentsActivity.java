d4379a1074e47ad3df523a6c0e468b2cb581ce09
Jaye Li <2238179368@qq.com>
2018-03-22 20:59:31 +0800

    Initial commit

---
+++ app/src/main/java/pers/test/activity/ExcellentStudentsActivity.java
@@ -0,0 +1,15 @@
+package pers.test.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pers.test.R;
+
+public class ExcellentStudentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excellent_students);
+    }
+}
