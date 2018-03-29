cd8b0835d635bcd4a7696b79802bf18564320b8d
惠普 <508530366@qq.com>
2018-03-29 21:15:50 +0800

    新加公告布局与Activity

---
+++ app/src/main/java/pers/test/activity/NewDetailActivity.java
@@ -0,0 +1,16 @@
+package pers.test.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pers.test.R;
+
+
+public class NewDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_detail);
+    }
+}
