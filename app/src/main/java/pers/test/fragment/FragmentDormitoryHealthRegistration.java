fe61c6316bb286fd4b0dfa2f77a5f0aa8c3c3201
惠普 <508530366@qq.com>
2018-04-12 20:39:18 +0800

    修改部分图标

--- app/src/main/java/pers/test/fragment/FragmentDormitoryHealthRegistration.java
+++ app/src/main/java/pers/test/fragment/FragmentDormitoryHealthRegistration.java
@@ -1,12 +1,23 @@
 package pers.test.fragment;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import pers.test.R;
+
+import pers.test.model.Environment;
 
 /**
  * 宿舍 - 卫生登记
@@ -14,11 +25,92 @@ import pers.test.R;
 
 public class FragmentDormitoryHealthRegistration extends Fragment {
 
+    private RecyclerView myVisit;
+    List<Environment> data=new ArrayList<>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_dormitory_health_registration, null);
+        View view = inflater.inflate(R.layout.fragment_visit, null);
 
+        initView(view);
+        Random random = new Random();
+        String red;
+        for(int i=0;i<6;i++){
+            Environment e=new Environment();
+            e.setGrade("50");
+            e.setTip("卫生登记");
+            e.setName("xxx");
+            e.setAddress("1栋2室");
+            red = Integer.toHexString(random.nextInt(256)).toUpperCase();
+            e.setColor(red);
+            data.add(e);
+        }
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getContext());
+        myVisit.setLayoutManager(layoutManager);
+        MyAdapter myAdapter=new MyAdapter(getContext(),data);
+        myVisit.setAdapter(myAdapter);
         return view;
+    }
+
+    private void initView(View view) {
+        myVisit = (RecyclerView) view.findViewById(R.id.myVisit);
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        Context context;
+        List<Environment> data;
+
+        public MyAdapter(Context context, List<Environment> data) {
+            this.context = context;
+            this.data = data;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.en_item, parent, false);//暂时不将布局文件放入root
+
+           ViewHolder viewHolder = new ViewHolder(view);
+
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            holder.Tip.setText(data.get(position).getTip());
+            holder.Name.setText(data.get(position).getName());
+            holder.Time.setText(data.get(position).getTime());
+            holder.Address.setText(data.get(position).getAddress());
+            holder.Grade.setText(data.get(position).getGrade());
+            holder.Grade.setTextColor(Color.RED);
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+
+            public TextView Address;
+            public TextView Tip;
+            public TextView Name;
+            public TextView Time;
+            public TextView Grade;
+
+            public ViewHolder(View rootView) {
+                super(rootView);
+                this.Address = (TextView) rootView.findViewById(R.id.Address);
+                this.Tip = (TextView) rootView.findViewById(R.id.Tip);
+                this.Name = (TextView) rootView.findViewById(R.id.Name);
+                this.Time = (TextView) rootView.findViewById(R.id.Time);
+                this.Grade = (TextView) rootView.findViewById(R.id.Grade);
+            }
+        }
+
+
     }
 }
--- app/src/main/java/pers/test/fragment/FragmentDormitoryHostelVisits.java
+++ app/src/main/java/pers/test/fragment/FragmentDormitoryHostelVisits.java
@@ -1,12 +1,21 @@
 package pers.test.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pers.test.R;
+import pers.test.model.VisitInfo;
 
 /**
  * 宿舍 - 宿舍走访
@@ -14,11 +23,101 @@ import pers.test.R;
 
 public class FragmentDormitoryHostelVisits extends Fragment {
 
+    private RecyclerView myVisit;
+    List<VisitInfo> data=new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_dormitory_hostel_visits, null);
+        View view = inflater.inflate(R.layout.fragment_visit, null);
 
+        initView(view);
+        for(int i=0;i<5;i++){
+            VisitInfo v=new VisitInfo();
+            v.setContent("走访");
+            v.setAddress("成都");
+            v.setName("王小二");
+            v.setTime("2018");
+            v.setId("XXXXXX");
+            v.setRelative("父子");
+            v.setVisitor("xxx");
+            v.setTip("来访登记");
+            data.add(v);
+        }
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        myVisit.setLayoutManager(layoutManager);
+        MyAdapter myAdapter=new MyAdapter(getContext(),data);
+        myVisit.setAdapter(myAdapter);
         return view;
+    }
+
+    private void initView(View view) {
+        myVisit = (RecyclerView) view.findViewById(R.id.myVisit);
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        Context context;
+        List<VisitInfo> data;
+
+        public MyAdapter(Context context, List<VisitInfo> data) {
+            this.context = context;
+            this.data = data;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.visit_item, parent, false);//暂时不将布局文件放入root
+
+            ViewHolder viewHolder = new ViewHolder(view);
+
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.Visitor.setText(data.get(position).getVisitor());
+            holder.Id.setText(data.get(position).getId());
+            holder.Tip.setText(data.get(position).getTip());
+            holder.Name.setText(data.get(position).getName());
+            holder.Time.setText(data.get(position).getTime());
+            holder.Phone.setText(data.get(position).getPhone());
+            holder.record_content.setText(data.get(position).getContent());
+            holder.Address.setText(data.get(position).getAddress());
+            holder.Relative.setText(data.get(position).getRelative());
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView Visitor;
+            public TextView Tip;
+            public TextView Name;
+            public TextView Time;
+            public TextView Phone;
+            public TextView Relative;
+            public TextView Id;
+            public TextView Address;
+            public TextView record_content;
+
+
+            public ViewHolder(View rootView) {
+                super(rootView);
+
+                this.Visitor = (TextView) rootView.findViewById(R.id.Visitor);
+                this.Tip = (TextView) rootView.findViewById(R.id.Tip);
+                this.Name = (TextView) rootView.findViewById(R.id.Name);
+                this.Time = (TextView) rootView.findViewById(R.id.Time);
+                this.Phone = (TextView) rootView.findViewById(R.id.Phone);
+                this.Relative = (TextView) rootView.findViewById(R.id.Relative);
+                this.Id = (TextView) rootView.findViewById(R.id.Id);
+                this.Address = (TextView) rootView.findViewById(R.id.Address);
+                this.record_content = (TextView) rootView.findViewById(R.id.record_content);
+            }
+        }
+
+
     }
 }
