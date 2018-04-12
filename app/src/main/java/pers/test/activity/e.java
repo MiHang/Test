9c677b0c78f90d4fb2b2051df3e7f1b780bf54cf
惠普 <508530366@qq.com>
2018-04-12 19:14:45 +0800

    修改部分图标

--- app/src/main/java/pers/test/activity/e.java
+++ app/src/main/java/pers/test/activity/e.java
@@ -2,7 +2,13 @@ package pers.test.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import pers.test.R;
 
@@ -12,11 +18,42 @@ import pers.test.R;
 
 public class e extends AppCompatActivity {
 
+    @BindView(R.id.dormitory_back)
+    ImageView dormitoryBack;
+    @BindView(R.id.dormitory_title)
+    TextView dormitoryTitle;
+    @BindView(R.id.dormitory_icon)
+    ImageView dormitoryIcon;
+    @BindView(R.id.build)
+    Spinner build;
+    @BindView(R.id.room)
+    Spinner room;
+    @BindView(R.id.teacher)
+    Spinner teacher;
+    @BindView(R.id.work_kind)
+    Spinner workKind;
+    @BindView(R.id.year)
+    Spinner year;
+    @BindView(R.id.month)
+    Spinner month;
+    @BindView(R.id.day)
+    Spinner day;
+    @BindView(R.id.visit_content)
+    EditText visitContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dormitory_visiter);
         ButterKnife.bind(this);
+        dormitoryTitle.setText("走访记录");
+
+        dormitoryBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 
 }
