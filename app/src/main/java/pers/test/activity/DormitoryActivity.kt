2edc5a6a3e5e70152f284ac03724317f133f710e
Jaye Li <2238179368@qq.com>
2018-04-12 18:43:07 +0800

    Merge branch 'master' of C:\develop\Workspaces\Android\Test with conflicts.

---
+++ app/src/main/java/pers/test/activity/DormitoryActivity.kt
@@ -0,0 +1,14 @@
+package pers.test.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import pers.test.R
+
+class DormitoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dormitory)
+    }
+
+}
