9fc6abedbf8e90f2fc3863465766423f0338c54e
Jaye Li <2238179368@qq.com>
2018-04-16 20:25:25 +0800

    微调布局及添加卫生公示跳转丽链接

--- app/src/main/java/pers/test/fragment/FragmentDormitoryHealthPublicity.java
+++ app/src/main/java/pers/test/fragment/FragmentDormitoryHealthPublicity.java
@@ -1,6 +1,7 @@
 package pers.test.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,12 +10,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import pers.test.R;
+import pers.test.activity.HealthPublicityDetailActivity;
+import pers.test.callback.DormitoryClickItemListener;
 import pers.test.model.Environment;
 
 /**
@@ -32,14 +36,21 @@ public class FragmentDormitoryHealthPublicity extends Fragment {
 
         initView(view);
         for(int i=0;i<6;i++){
-
-
             data.add("2018年");
         }
         RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getContext());
         myVisit.setLayoutManager(layoutManager);
         MyAdapter myAdapter=new MyAdapter(getContext(),data);
         myVisit.setAdapter(myAdapter);
+
+        myAdapter.setDormitoryClickItemListener(new DormitoryClickItemListener() {
+            @Override
+            public void setOnClickItemListener(int position) {
+                Intent intent = new Intent(getActivity(), HealthPublicityDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 
@@ -47,9 +58,16 @@ public class FragmentDormitoryHealthPublicity extends Fragment {
         myVisit = (RecyclerView) view.findViewById(R.id.myVisit);
     }
 
+
     class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
         Context context;
         List<String> data;
+
+        private DormitoryClickItemListener dormitoryClickItemListener;
+        public void setDormitoryClickItemListener(DormitoryClickItemListener dormitoryClickItemListener) {
+            this.dormitoryClickItemListener = dormitoryClickItemListener;
+        }
 
         public MyAdapter(Context context, List<String> data) {
             this.context = context;
@@ -66,12 +84,15 @@ public class FragmentDormitoryHealthPublicity extends Fragment {
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-
+        public void onBindViewHolder(ViewHolder holder, final int position) {
             holder.Time.setText(data.get(position).toString());
-
+            holder.item_root.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dormitoryClickItemListener.setOnClickItemListener(position);
+                }
+            });
         }
-
 
         @Override
         public int getItemCount() {
@@ -80,17 +101,17 @@ public class FragmentDormitoryHealthPublicity extends Fragment {
 
         class ViewHolder extends RecyclerView.ViewHolder {
 
+            public LinearLayout item_root;
             public TextView Time;
             public ImageView More;
 
             public ViewHolder(View rootView) {
                 super(rootView);
+                this.item_root = (LinearLayout) rootView.findViewById(R.id.item_notice_root);
                 this.Time = (TextView) rootView.findViewById(R.id.Time);
                 this.More = (ImageView) rootView.findViewById(R.id.More);
             }
-
         }
-
-
     }
+
 }
